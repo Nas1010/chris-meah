9c27930edd417d0af4080d820f17d5e794d519c9
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-03-25 13:51:59 +0000

    Added styling to Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import HeroSection from './components/HeroSection'; 
 import Navbar from './components/Navbar';
 import SchoolOfCode from './components/SchoolOfCode';
+import Footer from './components/Footer';
 
 function App() {
     return (
         <>
             <Navbar/>
             <HeroSection />
-            <SchoolOfCode/>
+            <SchoolOfCode />
+            <Footer/>
       </>
     );
   }
