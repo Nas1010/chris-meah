3b6067bd76ff8173c69d47e9a124c55e1d402159
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-03-31 21:16:06 +0100

    Updated styling of Contact section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,34 @@
-import styles from '../styles/Navbar.module.css';
+import styles from "../styles/Navbar.module.css"
 
 const Navbar = () => {
   return (
-    <nav className={styles.navbar}>
-      <h1 className={styles.logo}>Chris Meah</h1>
-      <ul>
-        <li><a href='#home'>Home</a></li>
-        <li><a href='#schoolOfCode'>School Of Code</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href='#contact'>Contact</a></li>
-      </ul>
-    </nav>
-  );
-};
+    <>
+      <nav className={styles.navbar}>
+        <h1 className={styles.logo}>Chris Meah</h1>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#schoolOfCode">School Of Code</a>
+          </li>
+          <li>
+            <a href="#testimonials">Testimonials</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        
+        <div className={styles.navDecoration}>
+          {[...Array(20)].map((_, index) => (
+            <div key={index} className={styles.glowDot}></div>
+          ))}
+        </div>
+      </nav>
+    </>
+  )
+}
 
-export default Navbar;
+export default Navbar
+
