aab7369320a9fd199e6dbdb92f892aa511aba7b1
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-03-31 16:09:22 +0100

    Updated Reviews.jsx to include rating

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,18 +1,17 @@
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/splide/dist/css/splide.min.css';
-import { reviews } from './reviewsData';
-import styles from '../styles/Testimonials.module.css';
-import { useEffect } from 'react';
+import {Splide, SplideSlide} from "@splidejs/react-splide"
+import "@splidejs/splide/dist/css/splide.min.css"
+import {reviews} from "./reviewsData"
+import styles from "../styles/Testimonials.module.css"
+import {useEffect} from "react"
 
 const Testimonials = () => {
-
   useEffect(() => {
-    const prevArrow = document.querySelector(".splide__arrow--prev");
-    const nextArrow = document.querySelector(".splide__arrow--next");
-    
-    if (prevArrow) prevArrow.style.left = "-12rem";
-    if (nextArrow) nextArrow.style.right = "-12rem";
-  }, []);
+    const prevArrow = document.querySelector(".splide__arrow--prev")
+    const nextArrow = document.querySelector(".splide__arrow--next")
+
+    if (prevArrow) prevArrow.style.left = "-12rem"
+    if (nextArrow) nextArrow.style.right = "-12rem"
+  }, [])
 
   return (
     <section id="testimonials" className={styles.testimonialContainer}>
@@ -23,8 +22,16 @@ const Testimonials = () => {
 
       <div className={styles.sliderContainer}>
         <blockquote>
-          <img className={`${styles.topQuote} ${styles.quote}`} src={`${process.env.PUBLIC_URL}/blockquote.svg`} alt="quote" />
-          <img className={`${styles.bottomQuote} ${styles.quote}`} src={`${process.env.PUBLIC_URL}/blockquote.svg`} alt="quote" />
+          <img
+            className={`${styles.topQuote} ${styles.quote}`}
+            src={`${process.env.PUBLIC_URL}/blockquote.svg`}
+            alt="quote"
+          />
+          <img
+            className={`${styles.bottomQuote} ${styles.quote}`}
+            src={`${process.env.PUBLIC_URL}/blockquote.svg`}
+            alt="quote"
+          />
         </blockquote>
 
         <Splide
@@ -38,16 +45,27 @@ const Testimonials = () => {
         >
           {reviews.map((review) => (
             <SplideSlide key={review.id} className={styles.splideSlide}>
-              <img className={styles.reviewImg} src={review.image} alt="Reviewer" />
+              <img
+                className={styles.reviewImg}
+                src={review.image}
+                alt="Reviewer"
+              />
               <div className={styles.content}>
                 <p className={styles.text}>{review.text}</p>
                 <div className={styles.info}>
+                  {/* <div className={styles.rating}>
+                    <span className={styles.star}>&#9734;</span>
+                    <span className={styles.star}>&#9734;</span>
+                    <span className={styles.star}>&#9734;</span>
+                    <span className={styles.star}>&#9734;</span>
+                    <span className={styles.star}>&#9734;</span>
+                  </div> */}
                   <div className={styles.rating}>
-                    <span className={styles.star}>&#9734;</span>
-                    <span className={styles.star}>&#9734;</span>
-                    <span className={styles.star}>&#9734;</span>
-                    <span className={styles.star}>&#9734;</span>
-                    <span className={styles.star}>&#9734;</span>
+                    {[...Array(5)].map((_, index) => (
+                      <span key={index} className={styles.star}>
+                        {index < review.rating ? "★" : "☆"}
+                      </span>
+                    ))}
                   </div>
                   <p className={styles.user}>{review.name}</p>
                 </div>
@@ -57,7 +75,7 @@ const Testimonials = () => {
         </Splide>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Testimonials;
+export default Testimonials
--- src/components/reviewsData.js
+++ src/components/reviewsData.js
@@ -4,7 +4,8 @@ export const reviews = [
       image:
         '/jennifer.jpg',
       name: 'Bootcamper: Jennifer Yashi',
-      text: 'I was quite satisfied with how Chris taught how to face a task by drawing it on paper and breaking it down into smaller chunks BEFORE attempting to code.',
+    text: 'I was quite satisfied with how Chris taught how to face a task by drawing it on paper and breaking it down into smaller chunks BEFORE attempting to code.',
+      rating: 5,
     },
     {
       id: 2,
@@ -12,6 +13,7 @@ export const reviews = [
         'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       name: 'Jane Smith',
       text: 'From neural networks to AI ethics, Chris offers valuable insights and guidance that have been essential in my AI learning journey.',
+      rating: 5,
     },
     {
       id: 3,
@@ -19,6 +21,7 @@ export const reviews = [
         '/matthew.jpg',
       name: 'From Teacher to Full-time Developer: Matthew England',
       text: 'The boot camp was much harder than anticipated.The sheer amount of content to learn was at times overwhelming. However, Chris was there every step of the way to guide us through.',
+      rating: 5,
     },
     {
       id: 4,
@@ -26,6 +29,7 @@ export const reviews = [
         'https://plus.unsplash.com/premium_photo-1671823917954-dc943c1bd9df?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       name: 'Emily Davis',
       text: 'Whether discussing machine learning models or the ethical implications of AI, Chris makes everything clear and engaging. Highly recommend him for anyone looking to dive into AI!',
+      rating: 5,
     },
     {
       id: 5,
@@ -33,5 +37,6 @@ export const reviews = [
         'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       name: 'David Miller',
       text: "Chris has an exceptional grasp of AI and its many facets. His ability to explain intricate AI concepts and real-world applications with clarity is amazing.",
+      rating: 5,
     },
   ];
